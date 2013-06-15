671500d932387dddc67f083aca4f1ddaa94d7e32
Archana Sevak <ajsevak@oakland.edu>
2013-06-15 16:41:54 -0400

    Image feature added

--- Car_dealershipBen_changed/db/migrate/20130609220727_create_cars.rb
+++ Car_dealershipBen_changed/db/migrate/20130609220727_create_cars.rb
@@ -1,15 +1,14 @@
 class CreateCars < ActiveRecord::Migration
   def change
     create_table :cars do |t|
-      t.integer :id
       t.string :make
       t.string :model
       t.integer :year
       t.integer :miles
       t.string :style
       t.text :description
-      t.string :img_url
       t.string :color
+      #t.string :string
       t.integer :vin
       t.decimal :price
 
