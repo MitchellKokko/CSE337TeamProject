578469c4dd419a3a4c2639e65148acf2b75cb632
Archana Sevak <ajsevak@oakland.edu>
2013-06-18 18:48:13 -0400

    files

--- Car_dealershipBen/app/models/admin.rb
+++
@@ -1,3 +0,0 @@
-class Admin < ActiveRecord::Base
-  attr_accessible :email, :employee_ID, :first_name, :last_name, :password
-end
--- Car_dealershipBen/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-CarDealership::Application.config.secret_token = '12386a319ce381b4495945fc512e40250908d281c3770af8fc3ab5491414bd75d03aed323a9fabea9d7e940fe7055a0d9a97c79c85c7b2fbeed4fcc604ab15d4'
